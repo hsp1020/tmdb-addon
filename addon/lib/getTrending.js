6f8352c4b167af0d1e35b3c1c7239c6b74671773
hsp1020 <125277818+hsp1020@users.noreply.github.com>
2025-09-20 08:25:01 +0900

    Update getTrending.js

--- addon/lib/getTrending.js
+++ addon/lib/getTrending.js
@@ -3,9 +3,9 @@ const { TMDBClient } = require("../utils/tmdbClient");
 const moviedb = new TMDBClient(process.env.TMDB_API);
 const { getMeta } = require("./getMeta");
 
+
 async function getTrending(type, language, page, genre) {
   const media_type = type === "series" ? "tv" : type;
-  const genreList = await getGenreList(language, type);
   const ITEMS_PER_PAGE = 500;
   const TMDB_PAGE_SIZE = 20;  // TMDB는   한   페  이  지  에   20개  만   줌
   const PAGES_TO_FETCH = Math.ceil(ITEMS_PER_PAGE / TMDB_PAGE_SIZE);
